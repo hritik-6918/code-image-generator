74b0c88f1478088d0748e5c1064d21e32f6c8cd7
Hrithik kumar <hritikkumar09sgnv.grd@gmail.com>
2024-07-25 20:04:42 +0530

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,237 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+"use client";
+import { useEffect, useRef, useState } from "react";
+import { toPng } from "html-to-image";
+import hljs from "highlight.js";
+import javascript from "highlight.js/lib/languages/javascript";
 
 export default function Home() {
+  const elementRef = useRef(null);
+  const preRef = useRef(null);
+  const [theme, setTheme] = useState("");
+  const [darkToggle, setDarkToggle] = useState(true);
+  const [inputText, setInputText] = useState("");
+  const [dynamicPadding, setDynamicPadding] = useState("64");
+  const [backgroundColor, setBackgroundColor] = useState(
+    "linear-gradient(140deg, rgb(255, 99, 99), rgb(115, 52, 52))"
+  );
+
+  const handlePadding = (event) => {
+  
+    const { value } = event.target;
+    debugger;
+    setDynamicPadding(value);
+  };
+
+  const htmlToImageConvert = () => {
+    toPng(elementRef.current, { cacheBust: false })
+      .then((dataUrl) => {
+        const link = document.createElement("a");
+        link.download = "my-image-name.png";
+        link.href = dataUrl;
+        link.click();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    autoResize();
+  }, [inputText]);
+
+  const formatTextarea = () => {
+    hljs.registerLanguage("javascript", javascript);
+    const highlightedCode = hljs.highlight(inputText, {
+      language: "javascript",
+      registerLanguage: "javascript",
+    }).value;
+    return (
+      <pre
+        className="Editor_formatted__x4nkp hljs hljs-rest crimsonTheme"
+        ref={preRef}
+        dangerouslySetInnerHTML={{ __html: highlightedCode }}
+      ></pre>
+    );
+  };
+
+  function autoResize() {
+    const textarea2 = document.getElementById("myTextarea2");
+    textarea2.style.height = "auto";
+    textarea2.style.height = textarea2.scrollHeight + "px";
+  }
+
+  const themeChange = (value) => {
+    const preElement = document.querySelector(
+      "pre.Editor_formatted__x4nkp.hljs"
+    );
+    if (value === "Crimson") {
+      preElement.classList.remove("blueTheme");
+      preElement.classList.remove("purpleTheme");
+      preElement.classList.add("crimsonTheme");
+      setBackgroundColor(
+        "linear-gradient(140deg, rgb(255, 99, 99), rgb(115, 52, 52))"
+      );
+    } else if (value === "Purple") {
+      preElement.classList.remove("blueTheme");
+      preElement.classList.remove("crimsonTheme");
+      preElement.classList.add("purpleTheme");
+      setBackgroundColor(
+        "linear-gradient(140deg, rgb(165, 142, 251), rgb(233, 191, 248))"
+      );
+    } else {
+      preElement.classList.remove("purpleTheme");
+      preElement.classList.remove("crimsonTheme");
+      preElement.classList.add("blueTheme");
+      setBackgroundColor(
+        "linear-gradient(140deg, rgb(142, 199, 251), rgb(28, 85, 170))"
+      );
+    }
+  };
+  const handleColor = (event) => {
+    const { value } = event.target;
+    setTheme(value);
+    themeChange(value);
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="flex justify-center items-center mx-auto min-h-screen bg-black">
+      <div
+        ref={elementRef}
+        className={`${
+          dynamicPadding === "64"
+            ? "p-20"
+            : dynamicPadding === "128"
+            ? "p-28"
+            : dynamicPadding === "32"
+            ? "p-14"
+            : "p-8"
+        } bg-slate-200`}
+        style={{
+          backgroundImage: `${backgroundColor}`,
+        }}
+      >
+        <div
+          className=" min-w-max h-content min-h-28 rounded-xl px-2"
+          style={
+            darkToggle
+              ? { backgroundColor: "rgba(0,0,0,.75)" }
+              : { backgroundColor: "white" }
+          }
+        >
+          <div className="w-38 h-full	pt-4 font-medium text-white flex">
+            <div className="w-1/3 flex pl-2 pr-2">
+              <div className="w-3 h-3 bg-red-500 rounded-lg mr-1.5"></div>
+              <div className="w-3 h-3 bg-yellow-500 rounded-lg mr-1.5"></div>
+              <div className="w-3 h-3 bg-green-500 rounded-lg mr-1.5"></div>
+            </div>
+            <div
+              className="text-sm w-72 flex justify-center text-slate-400 mr-4"
+              contentEditable
+              style={{ outline: "none" }}
+            >
+              untitled-1
+            </div>
+          </div>
+          <div id="myTextarea2" className="Editor_editor__Jz9sW">
+            <textarea
+              id="myTextarea"
+              onInput={() => autoResize()}
+              value={inputText}
+              onChange={(e) => setInputText(e.target.value)}
+              autoComplete="off"
+              autoCorrect="off"
+              spellCheck="false"
+              autoCapitalize="off"
             />
-          </a>
+            {formatTextarea()}
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="dashboard">
+        <div className="dashboard_items">
+          <strong className="dashboard_heading">Theme</strong>
+          <select
+            style={{
+              backgroundColor: "#191919",
+              color: "#959595",
+              border: " 1px solid #959595",
+            }}
+            onChange={handleColor}
+            className="block bg-black w-26 p-1  outline-none text-sm text-gray-900 border border-gray-300 rounded-lg  focus:ring-blue-500  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option
+              value="Purple"
+              style={{ backgroundColor: "#191919", color: "#959595" }}
+            >
+              Purple
+            </option>
+            <option
+              selected
+              value="Crimson"
+              style={{ backgroundColor: "#191919", color: "#959595" }}
+            >
+              Crimson
+            </option>
+            <option
+              value="Blue"
+              style={{ backgroundColor: "#191919", color: "#959595" }}
+            >
+              Blue
+            </option>
+          </select>
+        </div>
+        <div className="dashboard_items">
+          <strong class="dashboard_heading">Dark mode</strong>
+          <label className="inline-flex items-center cursor-pointer outline-none mt-2">
+            <input
+              type="checkbox"
+              checked={darkToggle}
+              value={darkToggle}
+              onChange={() => {
+                setDarkToggle(!darkToggle);
+              }}
+              className="sr-only peer outline-none"
+            />
+            <div className="relative w-9 h-5 outline-none bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          </label>
+        </div>
+        <div className="dashboard_items">
+          <strong className="dashboard_heading">Padding</strong>
+          <div className=" mt-2">
+            <div className="flex text-sm " style={{ color: "#959595" }}>
+              <button className={`mr-2 ${dynamicPadding==="16" ? "text-red-400":""}`} value={"16"} onClick={handlePadding}>
+                16
+              </button>
+              <button className={`mr-2 ${dynamicPadding==="32" ? "text-red-400":""}`} value={"32"} onClick={handlePadding}>
+                32
+              </button>
+              <button className={`mr-2 ${dynamicPadding==="64" ? "text-red-400":""}`} value={"64"} onClick={handlePadding}>
+                64
+              </button>
+              <button className={`mr-2 ${dynamicPadding==="128" ? "text-red-400":""}`} value={"128"} onClick={handlePadding}>
+                128
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="dashboard_items">
+          <strong className="dashboard_heading">Language</strong>
+          <div className="mt-1">
+            <span className="font-medium" style={{ color: "#fb8d8d" }}>
+              Javascript
+            </span>
+          </div>
+        </div>
+        <div className="dashboard_items">
+          <button
+            onClick={htmlToImageConvert}
+            className="bg-red-400 w-20 text-sm p-2 rounded-lg"
+          >
+            Save PNG
+          </button>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
